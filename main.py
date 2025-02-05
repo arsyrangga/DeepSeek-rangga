aff4cd0a81017488719242360421117915d2a9a1
rangga <arsyrangga@gmail.com>
2025-02-05 18:13:19 +0700

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
 
 model_name = "deepseek-ai/deepseek-llm-67b-base"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="auto",  offload_folder="./offload")
 model.generation_config = GenerationConfig.from_pretrained(model_name)
 model.generation_config.pad_token_id = model.generation_config.eos_token_id
 
